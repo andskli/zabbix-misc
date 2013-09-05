881fd2d903c1b59b1b8b980287abbe6b9cc71d3d
Andreas <andreas.skarmutsoslindh@gmail.com>
2013-09-05 13:34:15 +0200

    Create zabbix_discoverdisks.py

---
+++ zabbix_discoverdisks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Forced to use ugly/dumb json formatting
+
+fh = open("/proc/diskstats","r")
+
+disklist = []
+for l in fh:
+   l = l.split()
+   disklist.append(l[2])
+
+print "{"
+print "\t\"data\":["
+while disklist:
+   print "\t{"
+   print "\t\t\"{#DISKNAME}\":\"%s\"" % disklist.pop()
+   if len(disklist) > 0:
+      print "\t},"
+   else:
+      print "\t}"
+
+print "\t]"
+print "}"
+
+fh.close()
