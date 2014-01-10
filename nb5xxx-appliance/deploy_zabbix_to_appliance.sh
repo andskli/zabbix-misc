7b43e680934a0c7254510b635ab023f69452b10f
ondmagi <andreas.skarmutsoslindh@gmail.com>
2014-01-10 16:02:15 +0100

    update zbxdeployscript

--- nb5xxx-appliance/deploy_zabbix_to_appliance.sh
+++ nb5xxx-appliance/deploy_zabbix_to_appliance.sh
@@ -6,9 +6,9 @@
 #
 #
 # Author: Andreas Skarmutsos Lindh <andreas.skarmutsoslindh@gmail.com>
-# 
+#
 
-MASTER=XXX.XXX.XXX.XXX
+MASTER=XXX.IP
 
 pkill -9 zabbix_agentd
 
@@ -28,14 +28,15 @@ chown -R zabbix:zabbix /var/log/zabbix
 mkdir /etc/zabbix
 chown -R zabbix:zabbix /etc/zabbix
 
-# Pull zabbix_discoverdisks.py from repo
 wget -O /usr/local/bin/zabbix_discoverdisks.py http://${MASTER}/deploy/zabbix_discoverdisks.py
 chown zabbix:zabbix /usr/local/bin/zabbix_discoverdisks.py
 chmod +x /usr/local/bin/zabbix_discoverdisks.py
 
+
+# Setup zabbix_agent.conf
 cat > /etc/zabbix/zabbix_agent.conf <<EOF
 Server=${MASTER}
-### DISK I/O
+### DISK I/O###
 UserParameter=custom.vfs.dev.disks,/usr/local/bin/zabbix_discoverdisks.py
 UserParameter=custom.vfs.dev.read.ops[*],cat /proc/diskstats | egrep \$1 | head -1 | awk '{print \$\$4}'
 UserParameter=custom.vfs.dev.read.ms[*],cat /proc/diskstats | egrep \$1 | head -1 | awk '{print \$\$7}'
@@ -45,30 +46,21 @@ UserParameter=custom.vfs.dev.io.active[*],cat /proc/diskstats | egrep \$1 | head
 UserParameter=custom.vfs.dev.io.ms[*],cat /proc/diskstats | egrep \$1 | head -1 | awk '{print \$\$13}'
 #UserParameter=custom.vfs.dev.read.sectors[*],cat /proc/diskstats | egrep \$1 | head -1 | awk '{print \$6}'
 #UserParameter=custom.vfs.dev.write.sectors[*],cat /proc/diskstats | egrep \$1 | head -1 | awk '{print \$10}'
+### DISK I/O###
 EOF
 
-# Setup zabbix restarter from cron
-cat > /etc/cron.hourly/zabbixagent_restarter.sh <<EOF
-#!/bin/bash
-pkill zabbix_agentd && pkill zabbix_agentd
-/usr/local/bin/zabbix_agentd -c /etc/zabbix/zabbix_agent.conf
-EOF
-chmod 755 /etc/cron.hourly/zabbixagent_restarter.sh
-
-# Setup daily zabbix redeploy from cron
+# Setup daily redeploy of self from cron
 cat > /etc/cron.daily/zabbixagent_redeploy.sh <<EOF
 #!/bin/bash
 wget -O - http://${MASTER}/deploy/deploy_zabbix_to_appliance.sh | sh -
 EOF
 chmod 755 /etc/cron.daily/zabbixagent_redeploy.sh
 
-# Start zabbix agent and enable it in rc.local
 /usr/local/bin/zabbix_agentd -c /etc/zabbix/zabbix_agent.conf
 if ! grep -q "zabbix_agentd" /etc/rc.d/rc.local; then
     echo "/usr/local/bin/zabbix_agentd -c /etc/zabbix/zabbix_agent.conf" >> /etc/rc.d/rc.local
 fi
 
-# Fix PureDisk iptables firewall
 if test -f /etc/puredisk/custom_iptables_rules; then
     if ! grep -q "10050" /etc/puredisk/custom_iptables_rules; then
         echo -e "tcp 0.0.0.0/0 10050" >> /etc/puredisk/custom_iptables_rules
